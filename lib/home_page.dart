f6f15c1d78bb1f92fc6844fb6ae4395d7f22eb2e
Jody Roberto Battistini <jodi.battistini@libero.it>
2023-03-19 20:13:50 +0100

    feat(home-page): clear

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   final String selectedName;
+
   const HomePage({Key? key, required this.selectedName}) : super(key: key);
+
   @override
   _HomePage createState() => _HomePage();
 }
@@ -25,45 +26,55 @@ class _HomePage extends State<HomePage> {
       body: Center(
         child: currentIndex == 0
             ? Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.redAccent,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: Colors.amber,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      buttonName = '$selectedName Clicked';
-                    });
-                  },
-                  child: Text(buttonName),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      buttonName = 'Click Me';
-                    });
-                  },
-                  child: const Text('Reset'),
-                ),
-                Text(buttonName),
-              ],
-            ))
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.redAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.amber,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonName = '$selectedName Clicked';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Click Me';
+                        });
+                      },
+                      child: const Text('Reset'),
+                    ),
+                    Text(buttonName),
+                  ],
+                ))
             : GestureDetector(
-            onTap: () {
-              setState(() {
-                _isClicked = !_isClicked;
-              });
-            },
-            child: _isClicked
-                ? Image.asset('images/download.png')
-                : Image.asset('images/first.png')),
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.blueAccent,
+                    child: Center(
+                      child: Form(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(selectedName),
+                          )
+                        ),
+                      ),
+                    )),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
